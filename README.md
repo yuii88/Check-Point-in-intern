9dc3a5627ff974f0fda25565dee9a7e1f9804a38
Pimlapas Mochan <pim8828@gmail.com>
2022-06-01 15:29:23 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# InternShip
+ file each day
---
+++ laravel_cart/app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    //
+    public function productList()
+    {
+        $products = Product::all();
+
+        return view('products', compact('products'));
+    }
+}
