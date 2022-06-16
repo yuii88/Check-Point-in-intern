a6ce78f2709e87796f9d7fac1805908d902501dc
Pimlapas Mochan <pim8828@gmail.com>
2022-06-16 14:05:23 +0700

    show profile image

--- basic/resources/views/admin/admin_profile_view.blade.php
+++ basic/resources/views/admin/admin_profile_view.blade.php
@@ -9,10 +9,13 @@
             <div class="col-lg-6">
 
                 <!-- Simple card -->
-                <div class="card">
+                <div class="card"><br><br>
 <center>
-                    <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/small/img-1.jpg') }}" alt="Card image cap">
-</center>
+                    <img class="rounded-circle avatar-xl" 
+                    src="{{ (!empty($adminData->profile_image)) ? url('upload/
+                    admin_images/'.$adminData->profile_image):url('upload/no_image.jpg') }}" alt="Card image cap">
+        <!--test-->            <img class="rounded-circle avatar-xl" src="{{ url('upload/admin_images/'.$adminData->profile_image) }}">
+</center> 
                     <div class="card-body">
                         <h4 class="card-title">Name : {{ $adminData->name }}</h4>
                         <hr>
